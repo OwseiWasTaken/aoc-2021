1dc1721ac640126c9dc05acc717d22d7efcdd439
owsei <82065191+OwseiWasTaken@users.noreply.github.com>
2021-12-06 12:51:31 -0300

    Update 1.py

--- 4/1.py
+++ 4/1.py
@@ -1,5 +1,39 @@
-with open("input", 'r') as file:
-    input = file.readlines()
+from sys import stderr
 
-nums = list(map(int, input[0][:-1].split(','))) # pop(0) for next
-print(nums)
+with open("input", 'r') as file:
+    input = list(map(lambda x : x.strip(), file.readlines()))
+
+# RNs
+nums = list(map(int,
+	input.pop(0).split(',')
+)) # pop(0) for next
+
+boards = [
+	[[], [], [], [], []]
+]*(len(input)//6)
+
+bc = -1# board count
+
+while input:
+	line = input.pop(0)
+	if line:
+		stderr.write("bruh: %s, %d\n" % (line, bc))
+	else:
+		boards[bc] = [
+			input.pop(0),
+			input.pop(0),
+			input.pop(0),
+			input.pop(0),
+			input.pop(0),
+		]
+		print(boards[bc])
+		bc+=1
+
+# get n from nums
+def rng() -> int:
+	global nums
+	if nums:
+		return nums.pop(0)
+	else:
+		return -1
+
